47df3abbdf4c87fd359a42b20c2e98c72c993405
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-03-30 19:10:45 +0900

    Personクラスの実装を通じたカプセル化の学習 完了

--- src/person/Person.java
+++ src/person/Person.java
@@ -18,6 +18,54 @@ class Person {
         this.birthYear = birthYear;
     }
 
+    // getter / setter　の実装
+
+    // firstNameのgetterメソッド
+    public String getFirstName(){
+        return this.firstName;
+    } 
+
+    // firstNameのsetterメソッド
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    // lastName getter
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    // lastName setter
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    // heightM getter
+    public double getHeightM(){
+        return this.heightM;
+    }
+
+    // heightM setter
+    public void setHeightM(){
+        this.heightM = heightM;
+    }
+
+    // weightKg getter
+    public double getWeightKg(){
+        return this.weigthKg;
+    }
+
+    // weightKg setter
+    public void setWeightKg(){
+        this.weigthKg = weigthKg;
+    }
+
+    // birthYear getter
+    // birthYearは変更不可にするためsetterメソッドを実装しない
+    public int getBirthYear(){
+        return this.birthYear;
+    }
+
     // 内部のprivate変数の値を取得する
     // privateで宣言されたプロパティ（フィールド）は、同じクラス内のメソッドを通じて変更可能
     @Override
--- test/person/Test.java
+++ test/person/Test.java
@@ -3,11 +3,23 @@ package person;
 class Test{
     public static void main(String[] args){
         Person masa = new Person("Masa", "Sat", 1.77, 67.5, 1990);
-        // PersonオブジェクトのtoStringメソッドを呼び出し
+        // masaの情報と年齢を表示
+        System.out.println(masa);
+        System.out.println(masa.getAge());
+
+        // setterを使い、masaの名前を変更
+        masa.setFirstName("MasaII");
+        masa.setLastName("Kat");
+
+        // 変更後のmasaの情報を表示
+        System.out.println("変更後の情報を表示");
         System.out.println(masa);
 
-        // 年齢を取得
-        System.out.println(masa.getAge());
+        // 身長・体重・生年月日を表示
+        System.out.println();
+        System.out.println("身長: " + masa.getHeightM());
+        System.out.println("体重: " + masa.getWeightKg());
+        System.out.println("生年月日: " + masa.getBirthYear());
 
         // 以下のコードはクラスが異なるためエラーが発生する
         // mainメソッドがあっても、TestクラスかPersonクラスのプロパティやメソッドへはアクセスできない
@@ -15,7 +27,7 @@ class Test{
         // masa.lastName = "John";
 
         // 名前を変更するpublicメソッドを呼び出し
-        masa.changeName("kazu","kat");
-        System.out.println(masa);
+        // masa.changeName("kazu","kat");
+        // System.out.println(masa);
     }
 }
