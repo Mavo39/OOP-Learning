5c483f36eb6e9f7320f2572ac107edcb76e1e54a
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-03-31 22:14:15 +0900

    学習記録を追加

--- src/wallet/person.java
+++ src/wallet/person.java
@@ -1,22 +1,33 @@
 package wallet;
 
 class Person{
-    public String firstName;
-    public String lastName;
-    public int age;
-    public double heightM;
-    public double weightKg;
-    public Wallet wallet;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double heightM;
+    private double weightKg;
+    private String denomination;
+    private Wallet wallet;
 
-    public Person(String firstName, String lastName, int x, double y, double z){
+    public Person(String firstName, String lastName, int age, double heightM, double weightKg){
         this.firstName = firstName;
         this.lastName = lastName;
-        // "this"はメソッド内で必ずしも使用される必要はないようです
-        age = x; // ageの状態がxにアップグレード
-        heightM = y;
-        weightKg = z;
-        // Personオブジェクトを作成すると、Walletオブジェクトを持つことになる
-        wallet = new Wallet(); // デフォルトの財布を設定
+        this.age = age;
+        this.heightM = heightM;
+        this.weightKg = weightKg;
+    }
+
+    public Person(String firstName, String lastName, int age, double heightM, double weightKg, int initialMoney){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.heightM = heightM;
+        this.weightKg = weightKg;
+        
+    }
+
+    public String getFullName(){
+        return this.firstName + " " + this.lastName;
     }
 
     // 所持金を取得するメソッド
@@ -30,17 +41,38 @@ class Person{
         return this.wallet.getTotalMoney();
     }
 
-    // Personオブジェクトの状態を出力するメソッド
-    public void printState(){
-        System.out.println("firstname - " + firstName);
-        System.out.println("lastname - " + lastName);
-        System.out.println("age - " + age);
-        double weightKg = 70; // ローカル変数の優先度が高い
-        System.out.println("height - " + heightM + ", joking it is " + this.heightM);
-        System.out.println("weight - " + weightKg);
-        System.out.println("Current Money - " + getCash());
-        System.out.println();
+    public int[] getPayed(int money){
+
     }
+
+    public int[] spendMoney(int money){
+
+    }
+
+    public Wallet dropWallet(){
+        this.wallet = null;
+        return this.wallet;
+    }
+
+    public void addWallet(Wallet wallet){
+        this.wallet = wallet;
+    }
+
+    public void setDenominationPreference(String denomination){
+        
+    }
+
+
+    // Personオブジェクトの状態を出力するメソッド
+    // public void printState(){
+    //     System.out.println("firstname - " + firstName);
+    //     System.out.println("lastname - " + lastName);
+    //     System.out.println("age - " + age);
+    //     double weightKg = 70; // ローカル変数の優先度が高い
+    //     System.out.println("height - " + heightM + ", joking it is " + this.heightM);
+    //     System.out.println("weight - " + weightKg);
+    //     System.out.println("Current Money - " + getCash());
+    //     System.out.println();
+    // }
 }
 
-// コンストラクタ関数でwalletの初期化をしていないのにthis.walletにアクセスできるのは、クラスのフィールド（メンバ変数）が自動的に初期化されるため
--- src/wallet/wallet.java
+++ src/wallet/wallet.java
@@ -1,15 +1,25 @@
 package wallet;
 
 class Wallet{
-    public int bill1;
-    public int bill5;
-    public int bill10;
-    public int bill20;
-    public int bill50;
-    public int bill100;
+    // 仕様に基づきprivate化
+    private int bill1;
+    private int bill5;
+    private int bill10;
+    private int bill20;
+    private int bill50;
+    private int bill100;
 
     // 引数なしでコンストラクタを実行
     public Wallet(){}
+
+    public Wallet(int bill1, int bill5, int bill10, int bill20, int bill50, int bill100){
+        this.bill1 = bill1;
+        this.bill5 = bill5;
+        this.bill10 = bill10;
+        this.bill20 = bill20;
+        this.bill50 = bill50;
+        this.bill100 = bill100;
+    }
 
     public int getTotalMoney(){
         return (1 * this.bill1) + (5 * this.bill5) + (10 * this.bill10) + (20 * bill20) + (50 * bill50) + (100 * bill100);
@@ -20,22 +30,49 @@ class Wallet{
     public int insertBill(int bill, int amount){
         switch(bill){
             case(1):
-                bill1 += amount;
+                this.bill1 += amount;
                 break;
             case(5):
-                bill5 += amount;
+                this.bill5 += amount;
                 break;
                 case(10):
-                bill10 += amount;
+                this.bill10 += amount;
                 break;
             case(20):
-                bill20 += amount;
+                this.bill20 += amount;
                 break;
             case(50):
-                bill50 += amount;
+                this.bill50 += amount;
                 break;
             case(100):
-                bill100 += amount;
+                this.bill100 += amount;
+                break;
+            default:
+                return 0;
+        }
+        return bill * amount;
+    }
+
+    public int removeBill(int bill, int amount){
+        int removedAmount = 0;
+        switch(bill){
+            case(1):
+                bill1 -= amount;
+                break;
+            case(5):
+                bill5 -= amount;
+                break;
+            case(10):
+                bill10 -= amount;
+                break;
+            case(20):
+                bill20 -= amount;
+                break;
+            case(50):
+                bill50 -= amount;
+                break;
+            case(100):
+                bill100 -= amount;
                 break;
             default:
                 return 0;
